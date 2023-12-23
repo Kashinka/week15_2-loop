556ef8a72fa9bcfc952943c81d839270cbecbc03
Kashinka <azalena@yandex.ru>
2023-12-23 21:03:58 +0300

    Commented task 2

--- task.js
+++ task.js
@@ -15,18 +15,18 @@
 
 // В следующей функции есть цикл for, который считает количество выбранных жанров в списке прокрутки (элемент <select> позволяет выбрать несколько элементов). Цикл for объявляет переменную i и задаёт ей значение 0. Также он проверяет, что i меньше количества элементов в элементе <select>, выполняет оператор if и увеличивает i на один после каждого прохода цикла. Напиши, каким должен быть цикл.
 
-function howMany(select) {
-    let numberSelected = 0;
-    for (let i = 0; i < select.options.length; i++) {
-    if (select.options[i].selected) {
-        numberSelected++;
-    }
-    }
-    return numberSelected;
-    }
+// function howMany(select) {
+//     let numberSelected = 0;
+//     for (let i = 0; i < select.options.length; i++) {
+//     if (select.options[i].selected) {
+//         numberSelected++;
+//     }
+//     }
+//     return numberSelected;
+//     }
 
-    let btn = document.getElementById("btn");
-    const total = document.querySelector('.total');
-    btn.addEventListener("click", function(){
-    total.innerHTML= 'Выбрано элементов: ' + howMany(document.selectForm.musicTypes);
-});
+//     let btn = document.getElementById("btn");
+//     const total = document.querySelector('.total');
+//     btn.addEventListener("click", function(){
+//     total.innerHTML= 'Выбрано элементов: ' + howMany(document.selectForm.musicTypes);
+// });
